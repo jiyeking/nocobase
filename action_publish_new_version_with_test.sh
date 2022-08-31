927aa40835ee66dd120697412cafadfdc50f3f85
jiyeking <jiyeking1@gmail.com>
2022-08-31 19:08:45 +0800

    Update action_publish_new_version_with_test.sh

--- action_publish_new_version_with_test.sh
+++ action_publish_new_version_with_test.sh
@@ -63,8 +63,8 @@ done
 
 lang_data=$(curl http://localhost:13000/api/app:getLang)
 echo $lang_data
-# there is something wrong ,yarn nocobase install --lang=zh-CN but actual is get en-US,so just test  {"data":{"lang":
-expect_lang_data='{"data":{"lang":'
+# there is something wrong ,yarn nocobase install --lang=zh-CN but actual is get en-US,so just test  lang
+expect_lang_data="lang"
 if [[ $lang_data =~ $expect_lang_data ]];then
   echo "publish test success"
 else
